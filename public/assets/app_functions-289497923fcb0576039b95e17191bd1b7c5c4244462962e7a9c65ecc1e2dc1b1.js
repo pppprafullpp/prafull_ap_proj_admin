c00a5bb3f6850804eb1bebdfe2ca5fecbc3be053
apoorv <apoorv.coder@live.com>
2017-01-30 17:18:49 +0530

    bugs update

---
+++ public/assets/app_functions-289497923fcb0576039b95e17191bd1b7c5c4244462962e7a9c65ecc1e2dc1b1.js
@@ -0,0 +1,47 @@
+
+    function approve_ad(id){
+      console.log(id);
+      $.ajax({
+      url: "/advertisements/approve_ads",
+      type:"post",
+      data: {
+      id:id
+      },
+      success: function(response) {
+        console.log(response)
+      }
+     })
+    }
+
+    function decline_ad(id){
+      console.log(id);
+      $.ajax({
+      url: "/advertisements/decline_ads",
+      type:"post",
+      data: {
+      id:id
+      },
+      success: function(response) {
+        console.log(response)
+      }
+     })
+    }
+
+    function make_changes_in_app_configurations(key,value) {
+      response ={};
+      $.ajax({
+        "async":false,
+        global:false,
+        url:"/app_configurations",
+        type:"put",
+        data:{
+          config_key:key,
+          config_value:value
+        },
+        success: function(res) {
+          response = res;
+        }
+      });
+      return response;
+    }
+;
---
+++ public/assets/custom-7665535bb340f860f9aff3158fad8cab05a91110547432aff9bca3743b61ab1f.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+
+$(document).ready(function(){
+    resizeContent();
+
+    $(window).resize(function() {
+        resizeContent();
+    });
+
+    $(window).on("scroll",function() {
+        resizeContent();
+    });
+});
+
+function resizeContent() {
+    $height = $(window).height()+ window.pageYOffset;
+		// if ($height < 850){
+		$('.sidebar').height($height-50);
+		// }
+
+}
+;
