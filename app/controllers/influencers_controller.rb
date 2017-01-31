71498452c9970ffd360262c18813c2f060b7e4d8
apoorv <apoorv.coder@live.com>
2017-01-31 18:13:06 +0530

    updates

--- app/controllers/influencers_controller.rb
+++ app/controllers/influencers_controller.rb
@@ -1,10 +1,18 @@
 class InfluencersController < ApplicationController
+  AC_TYPES = {"SAVING"=>1,"CURRENT"=>2}
+
   skip_before_filter :verify_authenticity_token, :only=>[:destroy]
   def destroy
     Influencer.find(params[:id]).destroy
      render :json => {
       success:true
     }
+  end
+
+  def index
+    @breadcrumb = {'Dashboard' => root_url, 'Influencer Details' => ''}
+    @influencer_data = Influencer.find(params[:id])
+    @financial_info = Influencer.find(params[:id]).influencer_financial_info
   end
 
 end
