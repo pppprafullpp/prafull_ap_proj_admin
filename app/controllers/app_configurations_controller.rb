ae74e01cb3390068ec2a515c0ef94bca23704adf
apoorv <apoorv.coder@live.com>
2017-02-15 16:29:06 +0530

    remove byebug

--- app/controllers/app_configurations_controller.rb
+++ app/controllers/app_configurations_controller.rb
@@ -2,7 +2,6 @@ class AppConfigurationsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def update
-    byebug
     AppConfiguration.find_by(:config_key=>params[:config_key]).update_attributes(:config_value=>params[:config_value])
     if params[:config_key] == "update_social_data"
       system "whenever --update-crontab"
